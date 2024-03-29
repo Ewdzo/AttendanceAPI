40bd23e9516024ce8e95b6396cfd4a9281a6f363
Enzo <enzowederma@gmail.com>
2024-03-29 12:46:02 -0300

    ✨ feat: adapt zod schemas

---
+++ src/app/schemas/student.schemas.ts
@@ -0,0 +1,62 @@
+import Zod from "zod";
+
+export const StudentCreateRequestSchema = Zod.object({
+    matricula: Zod
+        .string({ required_error: "Field matricula must compose request body." })
+        .length(11, { message: "Field matricula must be 11 characters long." })
+        .regex(/\d\d\d\d\dBSI[0-9]+/i, { message: "Field matricula must match UFU's pattern for Information System students." }),
+
+    name: Zod
+        .string({ required_error: "Field name must compose request body." })
+        .min(1, { message: "Field name must not be empty." }),
+
+    photo: Zod
+        .string({ required_error: "Field photo must compose request body." })
+        .url({ message: "Field photo must be filled with valid url." }),
+
+    attendance: Zod
+        .number({ required_error: "Field attendance must compose request body." })
+});
+
+export const StudentSearchRequestSchema = Zod.object({
+    name: Zod
+        .string()
+        .optional(),
+
+    matricula: Zod
+        .string()
+        .optional(),
+
+    attendance: Zod
+        .number()
+        .optional()
+});
+
+export const StudentUpdateRequestSchema = Zod.object({
+    name: Zod
+        .string()
+        .min(1, { message: "Field name must not be empty." })
+        .optional(),
+
+    photo: Zod
+        .string()
+        .url({ message: "Field photo must be filled with valid url." })
+        .optional(),
+
+    matricula: Zod
+        .string({ required_error: "Field matricula must compose request body." })
+        .length(11, { message: "Field matricula must be 11 characters long." })
+        .regex(/\d\d\d\d\dBSI[0-9]+/i, { message: "Field matricula must match UFU's pattern for Information System students." }),
+
+    attendance: Zod
+        .number()
+        .min(1, { message: "Field attendance must not be empty." })
+        .optional()
+});
+
+export const StudentRemoveRequestSchema = Zod.object({
+    matricula: Zod
+        .string({ required_error: "Field matricula must compose request body." })
+        .length(11, { message: "Field matricula must be 11 characters long." })
+        .regex(/\d\d\d\d\dBSI[0-9]+/i, { message: "Field matricula must match UFU's pattern for Information System students." })
+});
